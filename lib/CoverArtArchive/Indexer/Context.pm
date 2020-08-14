683b3b04cad2989eb9e2c9c978d66da2c3211b1d
Michael Wiencek <mwtuea@gmail.com>
2020-08-14 12:46:34 -0500

    Allow configuring where indexes should be uploaded to
    HACKING-CAA.md from the musicbrainz-server repo currently requires us to
    manually change the line in IAS3Request.pm to do this. Here I've added a
    way to do it via configuration only, with {bucket} and {file}
    placeholders.

--- lib/CoverArtArchive/Indexer/Context.pm
+++ lib/CoverArtArchive/Indexer/Context.pm
@@ -17,10 +17,12 @@ sub _build_s3 {
 
     my $config = $self->config->{caa};
 
-    Net::Amazon::S3->new(
+    my $s3 = Net::Amazon::S3->new(
         aws_access_key_id => $config->{public_key},
         aws_secret_access_key => $config->{private_key},
     );
+    $s3->{_caa_config} = $config;
+    $s3;
 }
 
 has lwp => (
@@ -38,6 +40,8 @@ has config => (
     clearer => 'reload_config',
 );
 
+my $DEFAULT_UPLOAD_URL = '//{bucket}.s3.us.archive.org{file}';
+
 sub _build_config {
     my $self = shift;
 
@@ -46,6 +50,8 @@ sub _build_config {
 
     my $opts_config = $self->opts_config;
     my $config = Config::Tiny->read($opts_config->{config});
+
+    $config->{upload_url} //= $DEFAULT_UPLOAD_URL;
 
     # Override settings from the config file with those specified on the
     # command line.
